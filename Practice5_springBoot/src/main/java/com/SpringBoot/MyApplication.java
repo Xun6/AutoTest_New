cc32ca34c339efec61310773ac964a4c01e3b8ba
Xun6 <13617920171@163.com>
2021-11-05 13:49:45 +0800

    运行springBoot官方程序demo

---
+++ Practice5_springBoot/src/main/java/com/SpringBoot/MyApplication.java
@@ -0,0 +1,26 @@
+package com.SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 这个是官方的演示demo
+ */
+@RestController
+@EnableAutoConfiguration
+public class MyApplication {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+
+}
+
+
