e2c6752cd140df03e8a958732e0b7aad329f3481
Xun6 <13617920171@163.com>
2021-11-05 17:56:30 +0800

    使用springboot开发返回cookies信息的get请求

--- Practice5_springBoot/src/main/java/com/SpringBoot/MyGetMethod.java
+++ Practice5_springBoot/src/main/java/com/SpringBoot/MyGetMethod.java
@@ -1,17 +1,25 @@
 package com.SpringBoot;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
- * 这是一个演示示例，模拟的get请求接口，能返回cookies信息
+ * 这是一个演示示例，模拟的get请求接口
  */
 @RestController
 public class MyGetMethod {
 
+    /**
+     * 演示能返回cookies信息的get请求
+     * @param response
+     * @return
+     */
     @RequestMapping(value = "/getCookies",method = RequestMethod.GET)  //设置访问路径,请求方法
     public String getmethod(HttpServletResponse response){
 
@@ -19,5 +27,66 @@ public class MyGetMethod {
         response.addCookie(cookie); // 添加cookie信息到响应信息
 
         return "恭喜你，cookies信息获取成功！！";
+    }
+
+
+    /**
+     * 演示携带cookies信息访问的get请求
+     * @return
+     */
+    @RequestMapping(value = "/get/with/cookie",method = RequestMethod.GET)  //设置访问路径,请求方法
+    public String getWithCookie(HttpServletRequest request){
+        Cookie[] cookies= request.getCookies(); //获取cookies请求参数
+        //判断是否为空
+        if(Objects.isNull(cookies)){
+            return "访问错误，请携带cookies信息！！！";
+        }
+        // 检查cookies信息
+        for(Cookie cookie : cookies){
+            if(cookie.getName().equals("login") && cookie.getValue().equals("true")){
+                return "恭喜你，访问成功了！！！";
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 演示一个需要携带参数访问的get请求  //方法一
+     * uri形式：/getWithParams?key=value&key=value
+     * @param start
+     * @param end
+     * @return
+     */
+    @RequestMapping(value = "/getWithParams",method = RequestMethod.GET) //设置访问路径,请求方法
+    public Map<String,Integer> getList(@RequestParam Integer start,
+                                       @RequestParam Integer end){
+
+        //设置响应返回信息
+        Map<String,Integer> list = new HashMap<>();
+        list.put("衣服",100);
+        list.put("裤子",200);
+        list.put("鞋子",300);
+
+        return list;
+    }
+
+    /**
+     * 演示一个需要携带参数访问的get请求  //方法二
+     * uri形式：/getWithParams/10/20
+     * @param start
+     * @param end
+     * @return
+     */
+    @RequestMapping(value = "/getWithParams/{start}/{end}") //设置访问路径,请求方法
+    public Map<String,Integer> getList_two(@PathVariable Integer start,
+                                       @PathVariable Integer end){
+
+        //设置响应返回信息
+        Map<String,Integer> mylist = new HashMap<>();
+        mylist.put("衣服",600);
+        mylist.put("裤子",200);
+        mylist.put("鞋子",300);
+
+        return mylist;
     }
 }
